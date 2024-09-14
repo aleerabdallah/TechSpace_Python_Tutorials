7fbf5366b0972cb164fb309ac7fd1d960a54d0f6
mulbahoplanojames <mulbahjamesoplano@gmail.com>
2024-09-14 19:55:23 +0200

    update the following files arithmetic-operators.mdx, assignment-operator.mdx, comparisn-operator.mdx

--- README.md
+++ README.md
@@ -1,17 +1,11 @@
-
 # Python Docs
 
-
 Welcome to the TechSpace Python Docs! This repository contains the documentation for Python from the TechSPace Team, created using Docusaurus.
-
 
 ## Project Structure
 
 Welcome to the Python Crash Course developed by the TechSpace Team! This repository contains the documentation for a comprehensive crash course on Python, designed to help beginners get up to speed with Python programming.
 
-
 ## Getting Started
 
 To get started with this documentation, follow these steps:
-
-
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,46 +1,53 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			favicon: '/src/assets/techspace.jpg',
-			title: 'Docs with Tailwind',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Introduction to Python',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Overview', slug: 'intro-to-python/overview' },
-						{ label: 'Introduction to Python', slug: 'intro-to-python/introduction'},
-						{label: 'Installation', slug: 'intro-to-python/installation'},
-					],
-				},
-				{
-					label: 'Basic Concepts',
-					autogenerate: { directory: 'basic-concepts' },
-				},
-				{
-					label: 'Intermediate Concepts',
-					autogenerate: { directory: 'reference' },
-				},
-				{
-					label: 'Advanced Concepts',
-					autogenerate: { directory: 'reference' },
-				},
+  integrations: [
+    starlight({
+      favicon: "/src/assets/techspace.jpg",
+      title: "TechSpace",
+      logo: {
+        src: "/src/assets/techspaceTrans.png",
+        alt: "TechSpace Logo",
+      },
+      social: {
+        github: "https://github.com/aleerabdallah/TechSpace_Python_Tutorials",
+      },
+      sidebar: [
+        {
+          label: "Introduction to Python",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Overview", slug: "intro-to-python/overview" },
+            {
+              label: "Introduction to Python",
+              slug: "intro-to-python/introduction",
+            },
+            { label: "Installation", slug: "intro-to-python/installation" },
+          ],
+        },
+        {
+          label: "Basic Concepts",
+          autogenerate: { directory: "basic-concepts" },
+        },
+        {
+          label: "Intermediate Concepts",
+          autogenerate: { directory: "reference" },
+        },
+        {
+          label: "Advanced Concepts",
+          autogenerate: { directory: "reference" },
+        },
 
-				// {
-				// 	label: 'Reference',
-				// 	autogenerate: { directory: 'reference' },
-				// },
-			],
-			customCss: ['./src/tailwind.css'],
-		}),
-		tailwind({ applyBaseStyles: false }),
-	],
+        // {
+        // 	label: 'Reference',
+        // 	autogenerate: { directory: 'reference' },
+        // },
+      ],
+      customCss: ["./src/tailwind.css"],
+    }),
+    tailwind({ applyBaseStyles: false }),
+  ],
 });
